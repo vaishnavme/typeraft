b235b793505930b97509fa9b3b8bfdd49ca895e6
Vaishnav <chandurkarvaishnav@gmail.com>
2025-03-08 23:57:34 +0530

    improve: placeholder content and typography support

--- src/components/editor/extensions/index.ts
+++ src/components/editor/extensions/index.ts
@@ -2,11 +2,13 @@ import StarterKit from "@tiptap/starter-kit";
 import Underline from "@tiptap/extension-underline";
 import CharacterCount from "@tiptap/extension-character-count";
 import TextAlign from "@tiptap/extension-text-align";
+import Typography from "@tiptap/extension-typography";
 
 const extensions = [
   StarterKit,
   Underline,
   CharacterCount,
+  Typography,
   TextAlign.configure({
     types: ["heading", "paragraph"],
   }),
