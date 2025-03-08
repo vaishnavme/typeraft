8bf7e8138af88fb15e9cd469aef61942317b42df
Vaishnav <chandurkarvaishnav@gmail.com>
2025-03-08 18:25:02 +0530

    improve: separate text alignment toolbar component

--- src/components/editor/editor-ui/toolbar.tsx
+++ src/components/editor/editor-ui/toolbar.tsx
@@ -3,11 +3,13 @@ import { Toggle } from "@/components/ui/toggle";
 import {
   BoldIcon,
   ClockIcon,
+  CodeIcon,
   ItalicIcon,
   ListIcon,
   ListOrderedIcon,
   MaximizeIcon,
   MinimizeIcon,
+  QuoteIcon,
   StrikethroughIcon,
   UnderlineIcon,
 } from "lucide-react";
@@ -15,31 +17,84 @@ import ThemeSwitch from "@/components/layout/theme-switch";
 import { useState } from "react";
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
+import BlockSelector from "./block-selector";
+import TextAlignSelector from "./text-align";
+
+interface ToolbarProps {
+  editor: Editor;
+}
+
+const Seperator = () => <div className="h-6 w-px bg-border" />;
 
 const basicToolbarOptions = [
   {
     label: "Bold",
+    value: "bold",
     icon: <BoldIcon />,
     onClick: (editor: Editor) => editor.commands.toggleBold(),
   },
   {
     label: "Italic",
+    value: "italic",
     icon: <ItalicIcon />,
     onClick: (editor: Editor) => editor.commands.toggleItalic(),
   },
   {
     label: "Underline",
+    value: "underline",
     icon: <UnderlineIcon />,
     onClick: (editor: Editor) => editor.commands.toggleUnderline(),
   },
   {
     label: "Strike",
+    value: "strike",
     icon: <StrikethroughIcon />,
     onClick: (editor: Editor) => editor.commands.toggleStrike(),
   },
 ];
 
-const Toolbar = ({ editor }: { editor: Editor }) => {
+const BasicTools = (props: ToolbarProps) => {
+  const { editor } = props;
+
+  return (
+    <>
+      {basicToolbarOptions.map((option) => (
+        <Toggle
+          key={option.label}
+          onClick={() => option.onClick(editor)}
+          pressed={editor.isActive(option.value)}
+        >
+          {option.icon}
+        </Toggle>
+      ))}
+    </>
+  );
+};
+
+const List = (props: ToolbarProps) => {
+  const { editor } = props;
+
+  return (
+    <>
+      <Toggle
+        onClick={() => editor.commands.toggleBulletList()}
+        pressed={editor.isActive("bulletList")}
+      >
+        <ListIcon />
+      </Toggle>
+      <Toggle
+        onClick={() => editor.commands.toggleOrderedList()}
+        pressed={editor.isActive("orderedList")}
+      >
+        <ListOrderedIcon />
+      </Toggle>
+    </>
+  );
+};
+
+const Toolbar = (props: ToolbarProps) => {
+  const { editor } = props;
+
   const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
 
   const toggleFullScreen = () => {
@@ -62,23 +117,31 @@ const Toolbar = ({ editor }: { editor: Editor }) => {
   const charactersCount = editor.storage.characterCount.characters();
 
   return (
-    <div className="rounded-lg border">
-      <div className="flex items-center justify-between gap-x-1 border-b p-2">
-        <div className="flex items-center justify-between gap-x-1">
-          {basicToolbarOptions.map((option) => (
-            <Toggle key={option.label} onClick={() => option.onClick(editor)}>
-              {option.icon}
-            </Toggle>
-          ))}
-          <Toggle onClick={() => editor.commands.toggleOrderedList()}>
-            <ListOrderedIcon />
+    <div className="rounded-xl border">
+      <div className="flex items-center justify-between flex-wrap gap-1 border-b p-2">
+        <div className="flex items-center gap-1 flex-wrap">
+          <BlockSelector editor={editor} />
+          <Seperator />
+          <BasicTools editor={editor} />
+          <Seperator />
+          <List editor={editor} />
+          <Seperator />
+          <Toggle
+            onClick={() => editor.commands.toggleBlockquote()}
+            pressed={editor.isActive("blockquote")}
+          >
+            <QuoteIcon />
           </Toggle>
-          <Toggle onClick={() => editor.commands.toggleBulletList()}>
-            <ListIcon />
+          <TextAlignSelector editor={editor} />
+          <Toggle
+            onClick={() => editor.commands.toggleCodeBlock()}
+            pressed={editor.isActive("codeblock")}
+          >
+            <CodeIcon />
           </Toggle>
         </div>
 
-        <div className="flex items-center gap-x-1">
+        <div className="flex items-center gap-1">
           <Button onClick={toggleFullScreen} variant="ghost" size="icon">
             {isFullScreen ? <MinimizeIcon /> : <MaximizeIcon />}
           </Button>
@@ -86,7 +149,7 @@ const Toolbar = ({ editor }: { editor: Editor }) => {
         </div>
       </div>
 
-      <div className="p-2 flex items-center justify-between">
+      <div className="px-2 py-4 flex items-center justify-between">
         <div></div>
         <div className="flex items-center gap-x-2 text-xs">
           <p>
@@ -95,7 +158,7 @@ const Toolbar = ({ editor }: { editor: Editor }) => {
           <p>
             <span className="font-medium">{charactersCount}</span> characters
           </p>
-          <div className="flex items-center gap-x-1">
+          <div className="flex items-center gap-1">
             <ClockIcon size={12} />
             <span className="font-medium">
               {Math.ceil(wordsCount / 200)}
--- src/components/editor/extensions/index.ts
+++ src/components/editor/extensions/index.ts
@@ -1,7 +1,15 @@
 import StarterKit from "@tiptap/starter-kit";
 import Underline from "@tiptap/extension-underline";
 import CharacterCount from "@tiptap/extension-character-count";
+import TextAlign from "@tiptap/extension-text-align";
 
-const extensions = [StarterKit, Underline, CharacterCount];
+const extensions = [
+  StarterKit,
+  Underline,
+  CharacterCount,
+  TextAlign.configure({
+    types: ["heading", "paragraph"],
+  }),
+];
 
 export default extensions;
