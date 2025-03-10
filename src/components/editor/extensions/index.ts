b048115adcec3c38229261876ed074a9b409ea43
Vaishnav <chandurkarvaishnav@gmail.com>
2025-03-10 21:12:55 +0530

    feat: added color highlighter support

--- src/components/editor/extensions/index.ts
+++ src/components/editor/extensions/index.ts
@@ -3,12 +3,16 @@ import Underline from "@tiptap/extension-underline";
 import CharacterCount from "@tiptap/extension-character-count";
 import TextAlign from "@tiptap/extension-text-align";
 import Typography from "@tiptap/extension-typography";
+import Link from "@tiptap/extension-link";
+import ColorHighlighter from "./ColorHighlighter";
 
 const extensions = [
   StarterKit,
   Underline,
   CharacterCount,
   Typography,
+  Link,
+  ColorHighlighter,
   TextAlign.configure({
     types: ["heading", "paragraph"],
   }),
