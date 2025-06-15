23a59198f34bbfcdd4f9abe22d4e1bd77660475d
Vaishnav Chandurkar <42497931+vaishnavme@users.noreply.github.com>
2025-06-15 21:20:49 +0530

    improve: app window layout, toggle full screen, minimize

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState, type ReactNode } from "react";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 import FontToggle from "./font-toggle";
 import ThemeToggle from "./theme-toggle";
 import store, { storeKeys } from "../../lib/store";
 import { useTheme } from "../../theme/theme-provider";
 import Button from "../ui/button";
 import SidePanel from "./side-panel";
+import ToggleFullScreen from "./toggle-fullscreen";
 
 interface LayoutProps {
   children: ReactNode;
@@ -34,19 +36,29 @@ const Layout = (props: LayoutProps) => {
   }, []);
 
   return (
-    <div className="relative antialiased geist w-full min-h-screen flex items-start">
+    <div
+      id="layout"
+      data-tauri-drag-region
+      className="relative antialiased geist w-full min-h-screen flex items-start p-4 !overflow-hidden"
+    >
       <main className="w-full">
         {children}
-        <footer className="fixed w-full left-0 bottom-0 flex items-center justify-between gap-x-2 px-4 opacity-0 h-12 hover:opacity-100 hover:bg-background transition-all ease-in-out text-accent cursor-default">
+        <footer className="fixed w-full left-0 bottom-0 flex items-center justify-between gap-x-2 px-4 opacity-0 h-12 hover:opacity-100 hover:bg-background transition-all ease-in-out text-accent cursor-default rounded-b-xl border-b-6 border-x-6 border-border">
           <div className="flex items-center gap-x-2">
             <FontToggle />
             •
             <ThemeToggle />
           </div>
 
-          <Button.MonoButton onClick={() => setShowDrawer(true)}>
-            ← Open
-          </Button.MonoButton>
+          <div className="flex items-center gap-x-2">
+            <Button.MonoButton onClick={() => setShowDrawer(true)}>
+              ← entries
+            </Button.MonoButton>
+            <ToggleFullScreen />
+            <Button.MonoButton onClick={() => getCurrentWindow().close()}>
+              close
+            </Button.MonoButton>
+          </div>
         </footer>
       </main>
 
