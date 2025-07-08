c1464a5d007163a89f1e648588b5da909371def6
Vaishnav <chandurkarvaishnav@gmail.com>
2025-07-08 20:10:02 +0530

    improve: added shadcn sheet component

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,7 @@
 export const lookup_cache = "lookup_cache.json" as const;
+
+export const font_class_map = {
+  geist: "font-geist",
+  "ibm-plex": "font-ibm-plex",
+  "geist-mono": "font-geist-mono",
+};
